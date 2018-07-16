1cf16f379efe27183aa7b5b871ac5fc7f25181fd
Aaron <aaronmckinstry706@gmail.com>
2018-07-16 08:58:51 +0000

    Added run script; added more basic CLI args for baseline/main.py.

--- scripts/baseline/main.py
+++ scripts/baseline/main.py
@@ -15,18 +15,19 @@ LOGGER = logging.getLogger(__name__)
 
 def run(clargs):
     LOGGER.info('Loading training data.')
-    training_data = pandas.read_csv('data/train.csv')
+    training_data = pandas.read_csv(str(clargs.training_data))
     training_data['log_target'] = numpy.log(training_data['target'] + 1.0)
 
     LOGGER.info('Fitting model.')
     linear_regression = linear_model.LinearRegression()
     linear_regression.fit(training_data.drop(columns=['log_target', 'target', 'ID']), training_data['log_target'])
-    training_data['log_prediction'] = linear_regression.predict(training_data.drop(columns=['log_target', 'target', 'ID']))
-    LOGGER.info('Training RMSLE: {0}'.format(math.sqrt(metrics.mean_squared_error(
-        training_data['log_target'], training_data['log_prediction']))))
+    training_data['log_prediction'] = linear_regression.predict(
+        training_data.drop(columns=['log_target', 'target', 'ID']))
+    rmsle = math.sqrt(metrics.mean_squared_error(training_data['log_target'], training_data['log_prediction']))
+    LOGGER.info('Training RMSLE: {0}'.format(rmsle))
 
     LOGGER.info('Loading testing data.')
-    testing_data = pandas.read_csv('data/test.csv')
+    testing_data = pandas.read_csv(str(clargs.testing_data))
 
     LOGGER.info('Making predictions.')
     log_targets = linear_regression.predict(testing_data.drop(columns=['ID']))
@@ -34,11 +35,9 @@ def run(clargs):
     testing_data['log_target'] = testing_data['log_target'].clip(lower=0.0, upper=math.log(4600000000000.0 + 1.0))
     testing_data['target'] = numpy.exp(testing_data['log_target']) - 1.0
 
-    output_dir = pathlib.Path('data')
-    output_dir.mkdir(parents=True, exist_ok=True)
-    output_path = output_dir / 'baseline_linear_regression.csv'
-    LOGGER.info('Outputting predictions to {}'.format(output_path))
-    testing_data[['ID', 'target']].to_csv(str(output_path), index=False)
+    clargs.output_path.parent.mkdir(parents=True, exist_ok=True)
+    LOGGER.info('Outputting predictions to {}'.format(clargs.output_path))
+    testing_data[['ID', 'target']].to_csv(str(clargs.output_path), index=False)
 
 
 def add_args(arg_parser: argparse.ArgumentParser):
@@ -50,11 +49,33 @@ def add_args(arg_parser: argparse.ArgumentParser):
                               'log(4600000000000 + 1)] (the largest transaction ever made, according to a quick '
                               'Google search, was 4.6 trillion dollars).')
 
-    def dir_path_arg(s: str):
-        p = pathlib.Path(s)
-        if not p.is_dir():
-            raise argparse.ArgumentTypeError('Path {} is not a directory.'.format(p))
+    def nonexistent_file_path(s: str):
+        try:
+            p = pathlib.Path(s)
+        except TypeError:
+            raise argparse.ArgumentTypeError('{} is not a valid path.'.format(s))
+        if p.exists():
+            raise argparse.ArgumentTypeError('Path {} must not exist.'.format(s))
         return p
 
-    arg_parser.add_argument('--output-dir', '-o', type=dir_path_arg, default='data',
-                            help='The directory to which you want the baseline predictions output. Default is "data".')
+    def existent_file_path(s: str):
+        try:
+            p = pathlib.Path(s)
+        except TypeError:
+            raise argparse.ArgumentTypeError('{} is not a valid path.'.format(s))
+        if not p.exists() or not p.is_file():
+            raise argparse.ArgumentTypeError('Path {} must be a file.'.format(s))
+        return p
+
+    arg_parser.add_argument('--output-path', '-o', type=nonexistent_file_path,
+                            default='predictions/baseline_linear_regression.csv',
+                            help='The directory to which you want the baseline predictions output. Default is '
+                                 '"predictions/baseline_linear_regression.csv".')
+    arg_parser.add_argument('--training-data', type=existent_file_path,
+                            default='data/train.csv',
+                            help='The path to the training data CSV file, downloaded from the Kaggle competition site. '
+                                 'Default is "data/train.csv".')
+    arg_parser.add_argument('--testing-data', type=existent_file_path,
+                            default='data/test.csv',
+                            help='The path to the testing data CSV file, downloaded from the Kaggle competition site. '
+                                 'Default is "data/test.csv".')
